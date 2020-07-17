84de2df6560637290d9c752b567ec4ce244c6d78
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2020-07-17 17:31:14 -0300

    Mejorado recorte de bloques.

--- CONTENTS.md
+++ CONTENTS.md
@@ -1,15 +1,28 @@
-alinear .................... alineado de texto                    *
-orientar ................... orientación automática de documentos *
-recortar ................... recorte de bloques de texto 
-clasificar_maquina ......... clasificación de máquina de escribir *
-clasificar_tipo ............ clasificación de tipo de documento   *
-detectar_sello ............. detección de sellos                  *
-ocr ........................ optical character recognition        *
-web_mobil .................. interfaz mobil de LUISA (web/app)
-comun ...................... material común a todos los proyectos 
-contrib .................... bibliotecas
-datos ...................... datos de prueba 
-docs ....................... documentos varios sobre el proyecto
-LICENSE .................... licencia de distribucion de este repositorio
-README.md .................. información básica sobre el proyecto
-CONTENTS.md ................ este archivo
+# Imagenes
+
+## Kit de desarrollo 
+* comun ...................... material común a todos los proyectos 
+* datos ...................... datos de prueba 
+* contrib .................... bibliotecas
+
+## Preprocesamiento
+* alinear .................... alineado de texto                  
+* orientar ................... orientación automática de documentos
+* recortar ................... recorte de bloques de texto 
+
+## Extraccion de info
+* detectar\_sello ............ detección de sellos                 
+* clasificar\_maquina ........ clasificación de máquina de escribir
+* clasificar\_tipo ........... clasificación de tipo de documento  
+* ocr ........................ optical character recognition      
+
+## Interfaz de usuario
+* web\_mobil ................. ajustar css o algo asi para disp. mobiles
+* app\_android ............... app de Android
+
+## Otras cosas
+* docs ....................... documentos varios sobre el proyecto
+* LICENSE .................... licencia de distribucion de este repositorio
+* README.md .................. información básica sobre el proyecto
+* CONTENTS.md ................ este archivo
+
--- recortar/recortar.py
+++ recortar/recortar.py
@@ -21,21 +21,20 @@ import functools # functional programming tools
 
 #---------------------------------------------------------------------------------------
 
-ROW_ABS_THRES = 100   # row has 2.5% of its pixels black
+ROW_ABS_THRES = 20    # 
 ROW_REL_THRES = 4     # 4 more pixels than local baseline
 COL_ABS_THRES = 1     # column has at least three pixels black
-MIN_WORD_SEP = 10     # minimum separation between two words
+MIN_WORD_SEP = 20     # minimum separation between two words
 BLOCK_ABS_THRES = 4   # minimum number of pixels in a valid block
 TRAZO=1.0
 COLOR=(0.0,0.2,0.4,0.1)
 COLORMAP = plt.get_cmap('cool')
 WIN_SIZE = 1024
-MIN_BAND_HEIGHT = 20  # smallest letter I found was about 23
+MIN_ROW_HEIGHT = 20  # smallest letter I found was about 23
 
 #---------------------------------------------------------------------------------------
 
 def imwrite(fname,img):
-    #img.save(fname,compression=COMP)
     img.save(fname)
 
 #---------------------------------------------------------------------------------------
@@ -52,7 +51,11 @@ def imread(fname):
 
 #---------------------------------------------------------------------------------------
     
-def detect_bands(rmt):
+def detect_bands(rmt, min_size):
+    '''
+    given a sequence of values, detects contiguous segments or 'bands' which are significantly
+    darker than their neighborhood.
+    '''
     dif = np.int8(rmt[1:]) - np.int8(rmt[:-1])
     upidx = np.flatnonzero(dif > 0) + 1
     dnidx = np.flatnonzero(dif < 0) + 1 
@@ -68,23 +71,48 @@ def detect_bands(rmt):
         upidx = upidx[:-1]
         
     bands = list(zip(upidx,dnidx))
-    bands = list( filter( lambda x: x[1]-x[0] > MIN_BAND_HEIGHT, bands ) )
+    #bands = list( filter( lambda x: x[1]-x[0] > min_size, bands ) )
     return bands
 
 
 
 #---------------------------------------------------------------------------------------
-def cut_blocks(img,debugdir):
 
+def detect_rows(img,debugfile):
+    # parameters of order filter
     radius  = 100
-    row_sum = np.sum(I,axis=1)
-    order   = int((radius*2+1)/3)
     domain  = np.arange(-radius,radius+1)
+    order   = int((radius*2+1)/3)
+
+    row_sum = np.sum(I,axis=1)
     row_sum_filtered    =  dsp.order_filter(row_sum,domain,order) 
     row_sum_thresholded = (row_sum > ROW_ABS_THRES) & ((row_sum - row_sum_filtered) > ROW_REL_THRES) #
-    bands = detect_bands(row_sum_thresholded)
-    band_heights = np.diff(bands)
+    row_list = detect_bands(row_sum_thresholded, MIN_ROW_HEIGHT)
+    band_heights = np.diff(row_list)
     h,hc = np.unique(band_heights,return_counts=True)
+    if debugfile is not None:
+        plt.close('all')
+        fig = plt.figure(figsize=(16,10))
+        plt.subplot(211)
+        plt.plot(row_sum,lw=0.5)
+        plt.plot(row_sum_filtered,lw=0.5)
+        plt.plot(100*row_sum_thresholded,lw=1)
+        plt.legend(('sum','filt','thres'))
+        for b in row_list:
+            plt.plot(b,(300,300),lw=1,color='black')
+        plt.grid(True)
+        plt.subplot(212)
+        plt.bar(h,hc)
+        plt.savefig(debugfile,dpi=300)
+    return row_list
+
+#---------------------------------------------------------------------------------------
+
+def detect_blocks(img,band_list,debugfile):
+    # parameters of order filter
+    radius  = 100
+    domain  = np.arange(-radius,radius+1)
+    order   = int((radius*2+1)/3)
     #
     # cada banda es luego cortada en bloques
     # para esto mide la intensidad promedio de cada columna en la banda
@@ -94,13 +122,12 @@ def cut_blocks(img,debugdir):
     # aqui se detectan los bloques en cada banda en base a las 
     # intensidades de las  columnas de pixeles
     #
-    block_map = (1-np.copy(I)).astype(float)
-    block_list    = list()
-    band_idx  = 0
+    block_list = list()
+    band_idx   = 0
     #
     # altura de las bandas
     #
-    for band in bands:
+    for band in band_list:
         y0 = band[0]
         y1 = band[1]
         band = I[y0:y1, :]
@@ -111,7 +138,7 @@ def cut_blocks(img,debugdir):
         # filtro de mediana para evitar separaciones entre palabras
         #
         col_sum_thresholded = dsp.medfilt(col_sum_thresholded, kernel_size=2*MIN_WORD_SEP+1)
-        col_blocks          = detect_bands(col_sum_thresholded)
+        col_blocks          = detect_bands(col_sum_thresholded, MIN_WORD_SEP)
         block_idx           = 0
         for block in col_blocks:
             x0 = block[ 0 ]
@@ -125,43 +152,121 @@ def cut_blocks(img,debugdir):
             if  (bsum >= BLOCK_ABS_THRES) & (bsum < Ib.size - BLOCK_ABS_THRES):
                 # mismo formato que modulo C : i0, j0, i1, j1, i0 orig, j0 orig, i1 orig, j0 orig, row idx, block idx
                 block_list.append(block_info)
-                block_map[y0:y1,x0:x1] = block_map[y0:y1,x0:x1] * 0.75
             block_idx = block_idx + 1
         band_idx = band_idx +1
-
-    if debugdir is not None:
-        plt.close('all')
-        fig = plt.figure(figsize=(9,12))
-        plt.subplot(221)
-        plt.plot(row_sum,lw=0.5)
-        plt.plot(row_sum_filtered,lw=0.5)
-        plt.plot(100*row_sum_thresholded,lw=1)
-        plt.legend(('sum','filt','thres'))
-
-        fimgbands  = os.path.join(debugdir,fbase + '_bands.png')
-        fimgblocks = os.path.join(debugdir,fbase + '_blocks.png')
-        png_file = os.path.join(debugdir,"_debug.png")
-        col_sum = np.sum(I,axis=0)
-        col_sum_filtered =  dsp.order_filter(col_sum,domain,order) 
-        col_sum_thresholded = (col_sum > ROW_ABS_THRES) & ((col_sum - col_sum_filtered) > ROW_REL_THRES) #
-        plt.subplot(222)
-        plt.plot(col_sum,lw=0.5)
-        plt.plot(col_sum_filtered,lw=0.5)
-        plt.plot(100*col_sum_thresholded,lw=0.5)
-        plt.legend(('sum','filt','thres'))
-        plt.subplot(223)
-        plt.bar(h,hc)
-        plt.subplot(224)
-        plt.imshow(img,cmap=cm.gray)
-        plt.savefig(png_file,dpi=300)
-        plt.show()
-        plt.close(fig)
-        block_file = os.path.join(debugdir,"block_map.png")
-        plt.imsave(block_file,(255*block_map).astype(np.uint8),cmap=cm.gray)
     #    
     # devolver resultado refinado
     #
     return block_list
+
+#---------------------------------------------------------------------------------------
+
+
+def label_block(img,block_info):
+    y0,x0,y1,x1 = block_info[2:]
+    w = x1 - x0
+    h = y1 - y0
+    aspect = w / h
+    block_img = img[y0:y1,x0:x1]
+    #
+    # trim block
+    #
+    ver_profile = np.sum(block_img,axis=1)
+    nz = np.flatnonzero(ver_profile)
+    top, bottom = nz[0], nz[-1]
+
+    hor_profile = np.sum(block_img,axis=0)
+    nz = np.flatnonzero(hor_profile)
+    left, right = nz[0], nz[-1]
+    trimmed_block = block_img[top:bottom,left:right] 
+    tw, th  = (right-left), (bottom-top)
+    tsize   = tw * th
+    taspect = tw / th
+    if th < h/4: # relative height w.r.t. non trimmed block
+        return 'flat'
+    if th < 20: # absolute height in pixels (smallest letter height ~ 20 pixels) 
+        return 'flat'
+    if tsize < 400: # 20x20 pixels is really small 
+        return 'small'
+    #
+    # aspect ratio too large: this could be due to a long underlined sentence
+    #
+    if taspect > 25: # too long for a single word
+        return 'long'
+    if np.sum(trimmed_block) > 0.9*tsize: # too dark
+        return 'dark'
+    return 'good'
+
+#---------------------------------------------------------------------------------------
+
+def refine_blocks(img,block_list,debugdir):
+    '''
+    El método primario  de recortar bloques es muy sencillo y rápido,
+    pero no es capaz de discernir si cada bloque es en sí relevante.
+    
+    Además puede haber bloques mal segmentados.
+    
+    Finalmente, se realizan estadísticas para detectar, por ejemplo
+    si la página debe ser girada -- si todos los bloques tienen más o menos
+    el mismo tamaño, eso es un fuerte indicador de que el documento está girado 90
+    grados.
+
+    Esta función devuelve dos listas: una con las coordenadas ajustadas y otra con etiquetas indicando
+    si los bloques deben borrarse, están mal segmentados, o sin dobles.
+    
+    Además devuelve un valor booleano indicando si la imagen debe ser girada
+
+    '''
+    #
+    # 1. marcamos bloques inútiles: vacíos, basura, o sólo con puntuación 
+    #
+    block_labels = [label_block(img,bi) for bi in block_list]
+    lab,freq = np.unique(block_labels,return_counts=True)
+    for i in range(len(lab)):
+        print(lab[i],freq[i])
+
+    return block_list,block_labels # for now, only this
+
+#---------------------------------------------------------------------------------------
+
+def create_block_map(img,block_list,block_labels):
+
+    block_map = np.ones((img.shape[0],img.shape[1],3))
+    block_map[:, :, 0 ] = (1-img).astype(float)
+    block_map[:, :, 1 ] = block_map[:,:,0 ]
+    block_map[:, :, 2 ] = block_map[:,:,0 ]
+
+    for i in range(len(block_list)):
+        info = block_list[i]
+        label = block_labels[i]
+        y0,x0,y1,x1 = info[2:]
+        w,h = (x1-x0),(y1-y0)
+        # for use with colormap 'RdYlGn' (red - yellow - green)
+        cmap = cm.get_cmap('hsv')
+        if label == 'good':
+            c = cmap(0.50)
+        elif label == 'long':
+            c = cmap(0.40) 
+        elif label == 'flat':
+            c = cmap(0.20) 
+        elif label == 'small':
+            c = cmap(0.10)
+        elif label == 'dark':
+            c = cmap(0)
+        else:
+            print('Unkown label',label)
+        block = block_map[y0:y1,x0:x1]
+        bred   = block[:,:,0]
+        bgreen = block[:,:,1]
+        bblue  = block[:,:,2]
+        bred[bred == 1] = c[0] 
+        bgreen[bgreen == 1] = c[1] 
+        bblue[bblue == 1] = c[2] 
+        block_map[y0:y1,x0:x1,0] = bred
+        block_map[y0:y1,x0:x1,1] = bgreen
+        block_map[y0:y1,x0:x1,2] = bblue
+
+    return block_map
 
 #---------------------------------------------------------------------------------------
 
@@ -178,6 +283,8 @@ if __name__ == '__main__':
 		    help="text file where input pre-aligned files are specified")
     ap.add_argument("-f","--force", action="store_true",
 		    help="Forces the output to be overwritten even if it exists.")
+    ap.add_argument("-m","--margin", type=int, default=100,
+		    help="Cut this number of pixels from each side of image before analysis.")
     #
     # INICIALIZACION
     #
@@ -210,7 +317,7 @@ if __name__ == '__main__':
             reldir,fname = os.path.split(relfname)
             fbase,fext   = os.path.splitext(fname)
             foutdir      = os.path.join(outdir,reldir)
-            debugdir     = os.path.join(foutdir,fbase + "_debug")
+            debugdir     = foutdir # os.path.join(foutdir,fbase + "_debug")
 
             fcsvblocks = os.path.join(foutdir,fbase + '.blocks')
             
@@ -228,12 +335,25 @@ if __name__ == '__main__':
 
             img = imread(os.path.join(prefix,relfname))
             I = 1 - np.array(img)
+            margin = args["margin"]
+            I = I[margin:-margin,margin:-margin]
+
             if not os.path.exists(debugdir):
                 os.makedirs(debugdir)
             #
-            # deteccion de bloques
+            # deteccion de bandas (renglones)
             #
-            block_list = cut_blocks(img,debugdir)
+            debugfile = os.path.join(foutdir,fbase + '_debug_rows.png')
+            row_list = detect_rows(img,debugfile)
+            #  
+            # deteccion de bloques primaria
+            #
+            fdebug = os.path.join(foutdir,fbase + '_debug_blocks.png')
+            block_list = detect_blocks(I,row_list,fdebug)
+            #
+            # refinamiento (se ve más de cerca cada bloque)
+            #
+            block_list, block_labels = refine_blocks(I,block_list,debugdir)
             #
             # guardamos lista de bloques en archivo CSV
             #
@@ -241,6 +361,12 @@ if __name__ == '__main__':
             for block_info in block_list:
                 print(functools.reduce(lambda a,b: str(a) + '\t' + str(b), block_info), file=fblocks)
             fblocks.close()
+            #
+            # generamos imagen de analisis
+            #
+            block_map = create_block_map(I,block_list,block_labels)
+            block_file = os.path.join(debugdir,fbase + "_blocks.png")
+            plt.imsave(block_file,block_map)
             #
             # fin loop principal
             #
